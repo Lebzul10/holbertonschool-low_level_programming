db3f9130ef10d4474425c238b5a4c4a98cfb5ce7
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-10-13 04:07:06 -0700

    a

---
+++ hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char: %z byte(s)", sizeof(char));
+printf("Size of a int: %z byte(s)", sizeof(int));
+printf("Size of a long int: %z byte(s)", sizeof(long int));
+printf("Size of a long long int: %z byte(s)", sizeof(long long int));
+printf("Size of a float: %z byte(s)", sizeof(float));
+return (0);
+}
