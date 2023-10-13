58487ec21d2347588343b401ad7f17bc406ca833
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-10-13 04:09:54 -0700

    a

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-printf("Size of a char: %z byte(s)", sizeof(char));
-printf("Size of a int: %z byte(s)", sizeof(int));
-printf("Size of a long int: %z byte(s)", sizeof(long int));
-printf("Size of a long long int: %z byte(s)", sizeof(long long int));
-printf("Size of a float: %z byte(s)", sizeof(float));
+printf("Size of a char: %zu byte(s)", sizeof(char));
+printf("Size of a int: %zu byte(s)", sizeof(int));
+printf("Size of a long int: %zu byte(s)", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)", sizeof(long long int));
+printf("Size of a float: %zu byte(s)", sizeof(float));
 return (0);
 }
