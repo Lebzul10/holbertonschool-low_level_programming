8a87e8467e479c60cb7387e6ad86a8478b0a85e1
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-10-16 07:59:09 -0700

    a

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -6,20 +6,24 @@
  */
 int main(void)
 {
-int i, j;
-for (i = '0'; i <= '9'; i++)
-{
-for (j = '0'; j <= '9'; j++)
-{
-if (i < j)
-{
-putchar(i);
-putchar(j);
-putchar(',');
-putchar(' ');
-}
-}
-}
-putchar('\n');
-return (0);
+	int i, j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			if (i < j)
+			{
+				putchar(i);
+				putchar(j);
+				if (i != '8' || (i == '8' && j != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	};
+	putchar('\n');
+	return (0);
 }
