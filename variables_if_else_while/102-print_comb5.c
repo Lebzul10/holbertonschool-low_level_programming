80e53cea3789ca97c5ec373f5c4aa84111ead98a
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-10-16 09:10:18 -0700

    a

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k, n;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			for (k = '0'; k <= '9'; k++)
+			{
+				for (n = '0'; n <= '9'; n++)
+				{
+					if (i < k || ((i + j) < (k + n) && i <= k))
+					{
+						putchar(i);
+						putchar(j);
+						putchar(' ');
+						putchar(k);
+						putchar(n);
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
