ea28d6a25c5513f840e15ebad5163cf001b067d7
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-10-17 11:22:28 -0700

    a

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - Something usefull
+ * @n: Something more usefull
+ *
+ * Return: Something much more usefull
+ */
+int _abs(int n)
+{
+  if (n < 0)
+	{
+	  return (-n);
+	}
+  else
+	{
+	  return (n);
+	}
+}
