1e145192ac782c7b2d200cc73eed54978015421e
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-10-18 07:57:40 -0700

    a

--- functions_nested_loops/100-main.c
+++ functions_nested_loops/100-main.c
@@ -4,41 +4,7 @@
  * main - check the code.
  *
  * Return: Always 0.
- */
-void times_hesen(int i, int j)
-{
-int x = j * i;
-                if (x >= 100)
-                {
-                    _putchar(',');
-                    _putchar(' ');
-                    _putchar(x / 100 + '0');
-                    _putchar((x / 10) % 10 + '0');
-                    _putchar(x % 10 + '0');
-                }
-                else if (x >= 10 && x <= 99)
-                {
-                    _putchar(',');
-                    _putchar(' ');
-                    _putchar(' ');
-                    _putchar(x / 10 + '0');
-                    _putchar(x % 10 + '0');
-                }
-                else if (x <= 9 && j > 0)
-                {
-                    _putchar(',');
-                    _putchar(' ');
-                    _putchar(' ');
-                    _putchar(' ');
-                    _putchar(x + '0');
-                }
-                else
-                {
-                    _putchar(x + '0');
-                }
-}
-
-
+ */                 
 int main(void)
 {
     print_times_table(3);
--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,11 +1,43 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * print_times_table - Something useful
  * @n: Something more useful
  *
  * Return: Something much more useful
  */
+void times_hesen(int i, int j)
+{
+	int x = j * i;
+
+	if (x >= 100)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(x / 100 + '0');
+		_putchar((x / 10) % 10 + '0');
+		_putchar(x % 10 + '0');
+	}
+	else if (x >= 10 && x <= 99)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(x / 10 + '0');
+		_putchar(x % 10 + '0');
+	}
+	else if (x <= 9 && j > 0)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(x + '0');
+	}
+	else
+	{
+		_putchar(x + '0');
+	}
+}
 void print_times_table(int n)
 {
 	int i, j;
