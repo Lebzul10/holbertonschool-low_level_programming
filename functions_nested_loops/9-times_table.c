9b9201f550ab7ba6d2e314d5a2234473b58d7442
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-10-18 03:53:38 -0700

    a

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -6,35 +6,32 @@
  */
 void times_table(void)
 {
-  int i, j, x;
+	int i, j, x;
 
-  for (i = 0; i <= 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
-	  for (j = 0; j <= 9; j++)
+		for (j = 0; j <= 9; j++)
 		{
-		  x = j * i;
-		  if (x >= 10)
-			{
-			  _putchar(',');
-              _putchar(' ');
-			  _putchar(x/10 + '0');
-			  _putchar(x%10 + '0');
-			 
-			  
-			}
-		  else if (x <= 9 && j > 0)
+			x = j * i;
+			if (x >= 10)
 			{
 				_putchar(',');
-              _putchar(' ');
-              _putchar(' ');	
-			  _putchar(x + '0');
-			  
- 			}
-		  else
+				_putchar(' ');
+				_putchar(x/10 + '0');
+				_putchar(x%10 + '0');
+			}
+			else if (x <= 9 && j > 0)
 			{
-			  _putchar(x + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');	
+				_putchar(x + '0');
+ 			}
+			else
+			{
+				_putchar(x + '0');
 			}
 		}
-	  _putchar('\n');
+		_putchar('\n');
 	}
 }
