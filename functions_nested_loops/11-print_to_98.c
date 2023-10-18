97913c8081b7d41c0ceb2b5b5ad4e9e96951f16f
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-10-18 06:12:26 -0700

    a

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -8,35 +8,34 @@
  */
 void print_to_98(int n)
 {
-  int i;
-  
-  if (n <= 98)
+	int i;
+
+	if (n <= 98)
 	{
-  	for (i = n; i <= 98; i++)
-	{
-	  if (i == 98)
+		for (i = n; i <= 98; i++)
 		{
-		  printf("%d\n", i);
-		}
-	  else
-		{
-		  printf("%d, ", i);
+			if (i == 98)
+			{
+				printf("%d\n", i);
+			}
+			else
+			{
+				printf("%d, ", i);
+			}
 		}
 	}
-	}
-  if ( n > 98)
+	if (n > 98)
 	{
-	for (i = n; i >= 98; i--)
-    {
-      if (i == 98)
-        {
-			 printf("%d\n", i);
-        }
-      else
-        {
-             printf("%d, ", i);
-        }
-	}
-	
+		for (i = n; i >= 98; i--)
+		{
+			if (i == 98)
+			{
+				printf("%d\n", i);
+			}
+			else
+			{
+				printf("%d, ", i);
+			}
+		}
 	}
 }
