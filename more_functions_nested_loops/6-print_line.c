1f23a35ba0227d343f8434d639f8fb24326f034a
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-10-21 09:19:51 -0700

    a

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -7,16 +7,16 @@
  */
 void print_line(int n)
 {
-  if (n <= 0)
+	if (n <= 0)
 	{
-	  _putchar('n');
+		_putchar('\n');
 	}
-  else
+	else
 	{
-	  while(n--)
+		while(n--)
 		{
-		  _putchar('_');
+			_putchar('_');
 		}
 	}
-  _putchar('\n');
+	_putchar('\n');
 }
