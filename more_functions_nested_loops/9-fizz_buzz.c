1d872ef5d3eb658cc6784e7163a257a096b34765
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-10-21 10:16:37 -0700

    a

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -8,23 +8,31 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if (i != 1)
 		{
-			printf("FizzBuzz");
-		}
-		else if (i % 3 == 0)
-		{
-			printf("Fizz");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz");
+			putchar(' ');
+			if (i % 3 == 0 && i % 5 == 0)
+			{
+				printf("FizzBuzz");
+			}
+			else if (i % 3 == 0)
+			{
+				printf("Fizz");
+			}
+			else if (i % 5 == 0)
+			{
+				printf("Buzz");
+			}
+			else
+			{
+				printf("%d", i);
+			}
 		}
 		else
 		{
-			printf("%d", i);
+			printf("%d");
 		}
 	}
 	putchar('\n');
