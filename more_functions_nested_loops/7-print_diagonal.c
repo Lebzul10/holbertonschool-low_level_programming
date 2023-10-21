b57dfd8ae66ef6bbdad9a8d31f3f7396f67199fa
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-10-21 09:48:39 -0700

    a

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_diagonal - Something usefull
+ * @n: Something more usefull
+ *
+ * Return: Something much more usefull
+ */
+void print_diagonal(int n)
+{
+	int a = 1;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\\');
+		while (a < n)
+		{
+			for (i = 1; i <= a; i++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			a++;
+		}
+		_putchar('\n');
+	}
+}
