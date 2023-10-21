afe46930266fca11df8fb52eaa6f8ced0a3d0b7b
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-10-21 08:25:43 -0700

    a

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - Something usefull
+ * @n: Something more usefull
+ *
+ * Return: Something much more usefull
+ */
+int _isupper(int c)
+{
+	if(c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
