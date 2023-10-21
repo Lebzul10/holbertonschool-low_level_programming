393e60bfdc4b653236b0cbcd30655154d63b7e51
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-10-21 09:03:57 -0700

    a

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - Something usefull
+ *
+ * Return: Something much more usefull
+ */
+void more_numbers(void)
+{
+	int i, a = 0;
+
+	while (a < 14)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+		a++;
+	}
+}
