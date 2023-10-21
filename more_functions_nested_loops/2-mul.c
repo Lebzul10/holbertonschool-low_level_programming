14bd66d9b8f5aef9652058850cef14c1d2c2284f
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-10-21 08:37:19 -0700

    a

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - Something usefull
+ * @a: Something more usefull
+ * @b: Something more usefull
+ *
+ * Return: Something much more usefull
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
