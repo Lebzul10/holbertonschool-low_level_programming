e59a367d741a8ee6a18a9da35e587059a7471680
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-10-23 05:33:08 -0700

    a

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * reset_to_98 - Something useful
+ * @n: Something more useful
+ *
+ * Return - Something much more useful
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	*a = *b;
+	*b = c;
+}
