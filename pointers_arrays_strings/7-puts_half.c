aa4bfa7e10aad63688378b160edc7dccbc374f7a
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-10-23 09:53:40 -0700

    a

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts_half - Something useful
+ * @str: Something more useful
+ *
+ * Return: Something much more useful
+ */
+void puts_half(char *str)
+{
+	int a = 0, c;
+
+	a = strlen(str);
+	for (c = a/2; c < a; c++)
+	{
+		_putchar(*(str + c));
+	}
+	_putchar('\n');
+}
