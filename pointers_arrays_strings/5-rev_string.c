9f074dbb6c458891a9f3522629c8b895b5dd99fc
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-10-23 08:38:31 -0700

    a

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rev_string - Something useful
+ * @s: Something more useful
+ *
+ * Return: Something much more useful
+ */
+void rev_string(char *s)
+{
+	int a = 0;
+	char *b;
+
+	while (*s != '\0')
+	{
+		*b = *s;
+		b++;
+		s++;
+		a++;
+	}
+	s--;
+	for (; a > 0; a--)
+	{
+		*s = *(b - a);
+		s--;
+	}
+  
+}
