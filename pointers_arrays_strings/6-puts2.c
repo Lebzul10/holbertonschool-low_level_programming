8f635e6df3af51b928a52a064c61b8b19066e370
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-10-23 09:30:41 -0700

    a

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -9,12 +9,13 @@ void puts2(char *str)
 {
 	int a = 0;
 
-	while (*(str + a) != '\0')
+	while (*str != '\0')
 	{
 		if (a % 2 == 0)
 		{
-		  _putchar(*(str + a));
+			_putchar(*str);
 		}
+		str++;
 		a++;
 	}
 	_putchar('\n');
