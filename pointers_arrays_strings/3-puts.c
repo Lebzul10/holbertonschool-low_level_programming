93d20831471b62ecd4da09a4a6c8a0764bc7c02c
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-10-23 06:06:54 -0700

    a

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * reset_to_98 - Something useful
+ * @n: Something more useful
+ *
+ * Return: Something much more useful
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
