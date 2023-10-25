65f5a6e49a69063391fa79530067e2751a3ec5f2
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-10-25 09:19:09 -0700

    a

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,21 +1,22 @@
 #include "main.h"
 #include <string.h>
 /**
- * reset_to_98 - Something useful
- * @n: Something more useful
+ * _strcat - Something useful
+ * @dest: Something more useful
+ * @src: Something more useful
  *
  * Return: Something much more useful
  */
 char *_strcat(char *dest, char *src)
 {
-  int a, b, i = 0;
+	int a, b, i = 0;
 
-  a = strlen(dest);
-  b = strlen(src);
-  while (i < b)
+	a = strlen(dest);
+	b = strlen(src);
+	while (i < b)
 	{
-	  *(dest + a + i) = *(src + i);
-	  i++;
+		*(dest + a + i) = *(src + i);
+		i++;
 	}
-  return (dest);
+	return (dest);
 }
