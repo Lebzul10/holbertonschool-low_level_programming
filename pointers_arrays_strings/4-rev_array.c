b0ee9c29e8a91e1cd06f095583893f2c83b6b24a
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-10-26 11:03:27 -0700

    a

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -10,12 +10,15 @@ void reverse_array(int *a, int n)
 {
 	int i = 0, b;
 
-	n = n - 1;
-	while (i <= n / 2)
+	if (n != 0)
 	{
-		b = *(a + (n - i));
-		*(a + (n - i)) = *(a + i);
-		*(a + i) = b;
-		i++;
+		n = n - 1;
+		while (i <= n / 2)
+		{
+			b = *(a + (n - i));
+			*(a + (n - i)) = *(a + i);
+			*(a + i) = b;
+			i++;
+		}
 	}
 }
