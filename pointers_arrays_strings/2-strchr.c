65c048431157738457cfe50e0ffbeaf1092d4b0f
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-10-29 10:20:43 -0700

    a

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strchr - Something usefull
+ * @s: Something more usefull
+ * @c: Something more usefull
+ *
+ * Return: Something much more usefull
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0, a = 0;
+
+	while (*(s + i) != c && *(s + i) != '\0')
+	{
+		i++;
+	}
+	while (*(s + a + i) != '\0')
+	{
+		return (s + a + i);
+	}
+	return (0);
+}
