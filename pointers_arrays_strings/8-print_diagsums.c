99d4c0ec1d53e623238510f5c9642b5a70d095a6
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-10-29 12:59:16 -0700

    a

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -9,12 +9,12 @@
 void print_diagsums(int *a, int size)
 {
   int i;
-  long a = 0, b = 0;
+  long c = 0, b = 0;
 
   for (i = 0; i < size; i++)
 	{
-		a += *(a + (size + 1) * i);
+		c += *(a + (size + 1) * i);
 		b += *(a + (size - 1) * (i + 1));
 	}
-	printf("%ld, %ld\n", a, b);
+	printf("%ld, %ld\n", c, b);
 }
