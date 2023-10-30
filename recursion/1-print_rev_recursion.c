c039ee769280f0f5cfce351f98b180cbdfac55ca
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-10-30 06:03:44 -0700

    a

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - Something useful
+ * @s: Something more useful
+ *
+ * Return: Something much more useful
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return; 
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
