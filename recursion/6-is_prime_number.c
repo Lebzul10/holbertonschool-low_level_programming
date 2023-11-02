b322873e0bc697619bc9c1a6c0e6ba8eaac08a5b
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-11-02 04:35:08 -0700

    a

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _prime - Something useful
+ * @a: Something more useful
+ * @b: Something more useful
+ *
+ * Return: Something much more useful
+ */
+int _prime(int a, int b)
+{
+	if (a % b != 0 && b < a)
+	{
+		return (_prime(a, b + 1));
+	}
+	else if ((a % b == 0 && b < a) || a <= 1)
+	{
+		return (0);
+ 	}
+	return (1);
+}
+/**
+ * is_prime_number - Something useful
+ * @n: Something more useful
+ *
+ * Return: Something much more useful
+ */
+int is_prime_number(int n)
+{
+	return (_prime(n, 2));
+}
