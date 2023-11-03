7f4bf29d425977863c0c187d3ddf4aa004729089
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-11-03 06:35:03 -0700

    a

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Something useful
+ * @argc: Something more useful
+ * @argv: Something more useful
+ *
+ * Return: Something much more useful
+ */
+int main(int argc, char *argv[])
+{
+	(void) *argv;
+	printf("%d\n", argc - 1);
+	return (0); 
+}
