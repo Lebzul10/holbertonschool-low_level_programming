32dae686742ae81eb258beb8df42e8f4101f5806
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-11-03 06:30:50 -0700

    a

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Something useful
+ * @argc: Something more useful
+ * @argv: Something more useful
+ *
+ * Return: Something much more useful
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
