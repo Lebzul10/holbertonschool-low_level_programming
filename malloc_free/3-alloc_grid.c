a656b65ff1ba2be818bb4458f2a06d6f35ee8dfd
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-11-13 09:58:49 -0800

    a

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+/**
+ * alloc_grid - Something usefull
+ * @height: Something more usefull
+ * @width: Something more usefull
+ *
+ * Return: Something much more usefull
+ */
+int **alloc_grid(int width, int height)
+{
+	int i = 0, j, **a;
+
+	a = malloc(height * sizeof(int *));
+	if (height <= 0 || width <= 0)
+	{
+		return (NULL);
+	}
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	while (i < height)
+	{
+		a[i] = malloc(width * sizeof(int));
+		if (a[i] == NULL)
+		{
+			free(a);
+			return (NULL);
+		}
+		i++;
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			a[i][j] = 0;
+		}
+	}
+	return (a);
+}
