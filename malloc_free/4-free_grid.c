f68990f2565805f4ba8b851f57ed9775701a5cb1
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-11-13 10:15:56 -0800

    a

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+/**
+ * free_grid - Something usefull
+ * @grid: Something more usefull
+ * @height: Something more usefull
+ *
+ * Return: Something much more usefull
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid[i]);
+}
