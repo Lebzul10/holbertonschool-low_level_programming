9a5fe2cc7db6882550f3acd7bd9e9a3f951e93e4
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-11-14 05:32:49 -0800

    a

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+/**
+ * malloc_checked - Something useful
+ * @b: Something more useful
+ *
+ * Return: Something much more useful
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *a;
+
+	a = malloc(b);
+	if (a == NULL)
+	{
+		exit(98);
+	}
+}
