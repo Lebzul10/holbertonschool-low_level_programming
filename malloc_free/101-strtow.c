07b3061a923d6fb2953a698ececca440f5cefb99
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-11-16 06:08:04 -0800

    a

--- malloc_free/101-strtow.c
+++ malloc_free/101-strtow.c
@@ -7,42 +7,43 @@
  */
 char **strtow(char *str)
 {
-    char **s;
-    int i = 0, j = 0, len_s = 0, row_count = 0, zeros = 0, a = 0, zeros_old = 0;
+	char **s;
+	int i, j = 0, len_s = 0, row_count = 0, zeros = 0, a = 0, zeros_old = 0, hoqa;
 
-    while (str[a] != '\0')
-    {
-	  if (str[a] != 32 && (str[a + 1] == 32 || str[a + 1] == '\0'))
-            row_count++;
-        a++;
-    }
+	while (str[a] != '\0')
+	{
+		if (str[a] != 32 && (str[a + 1] == 32 || str[a + 1] == '\0'))
+			row_count++;
+		a++;
+	}
 	if (row_count == 0)
-	  {
+	{
 		return (NULL);
-	  }
-    s = malloc((row_count + 1) * sizeof(char *));
-    for (i = 0; i < row_count; i++)
-    {
-        while (str[zeros + zeros_old] == 32)
-		  {
-            zeros++;
-		  }
-        while (str[len_s + zeros + zeros_old] != 32 && str[len_s + zeros + zeros_old] != '\0')
-        {
-            len_s++;
-        }
-        s[i] = malloc(sizeof(char) * len_s + 1);
-        while (j < len_s)
-        {
-            s[i][j] = str[j + zeros + zeros_old];
-            j++;
-        }
-        s[i][j] = '\0';
-        zeros_old += (len_s + zeros);
-        len_s = 0;
-        zeros = 0;
-        j = 0;
-    }
+	}
+	s = malloc((row_count + 1) * sizeof(char *));
+	for (i = 0; i < row_count; i++)
+	{
+		while (str[zeros + zeros_old] == 32)
+		{
+			zeros++;
+		}
+		hoqa = zeros + zeros_old;
+		while (str[len_s + hoqa] != 32 && str[len_s + hoqa] != '\0')
+		{
+			len_s++;
+		}
+		s[i] = malloc(sizeof(char) * len_s + 1);
+		while (j < len_s)
+		{
+			s[i][j] = str[j + zeros + zeros_old];
+			j++;
+		}
+		s[i][j] = '\0';
+		zeros_old += (len_s + zeros);
+		len_s = 0;
+		zeros = 0;
+		j = 0;
+	}
 	s[row_count] = NULL;
-    return s;
+	return (s);
 }
