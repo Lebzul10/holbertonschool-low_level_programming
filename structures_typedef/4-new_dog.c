e9d5e7782595d50bc05f606b2a3d7cebb71179b5
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-11-22 04:52:58 -0800

    a

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,36 +2,37 @@
 #include <string.h>
 #include "dog.h"
 /**
- * reset_to_98 - Something useful
- * @n: Something more useful
+ * new_dog - Something useful
+ * @name: Something more useful
+ * @age: Something more useful
+ * @owner: Something more useful
  *
  * Return: Something much more useful
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-  dog_t *new;
-  
-  new = malloc(sizeof(dog_t));
-  if (new == NULL)
-	{
-	  return (NULL);
-	}
-  new->name = malloc(strlen(name) + 1);
-  if (new->name == NULL)
-	{
-	  free(new);
-	  return (NULL);
-	}
-  strcpy(new->name, name);
-  new->age = age;
-  new->owner = malloc(strlen(owner) + 1);
-  if (new->owner == NULL)
-	{
-	  free(new->name);
-	  free(new);
-	  return (NULL);
-	}
-  strcpy(new->owner, owner);
+	dog_t *new;
 
-  return (new);	
+	new = malloc(sizeof(dog_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->name = malloc(strlen(name) + 1);
+	if (new->name == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	strcpy(new->name, name);
+	new->age = age;
+	new->owner = malloc(strlen(owner) + 1);
+	if (new->owner == NULL)
+	{
+		free(new->name);
+		free(new);
+		return (NULL);
+	}
+	strcpy(new->owner, owner);
+	return (new);
 }
