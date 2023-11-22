60b7a1865a863846e8fdad859e04efc02f338085
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-11-22 04:57:46 -0800

    a

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * reset_to_98 - Something useful
+ * @n: Something more useful
+ *
+ * Return: Something much more useful
+ */
+void free_dog(dog_t *d)
+{
+  free(d->name);
+  free(d->owner);
+  free(d->age);
+  free(d);
+  return (d);
+}
