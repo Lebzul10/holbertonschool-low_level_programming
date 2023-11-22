f4c094af212fc178954c9640dca851f5e4971e4d
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-11-22 03:34:52 -0800

    a

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,15 @@
+#ifndef DOGa
+#define DOGa
+/**
+ * struct dog - dog structure.
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: owner of the dog.
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
