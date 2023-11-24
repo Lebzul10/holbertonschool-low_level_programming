c7842b81852418d527dffdeeedcaebf3c226d899
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-11-24 04:43:55 -0800

    a

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+/**
+ * print_name - Something useful
+ * @name: Something more useful
+ * @f: Something more useful
+ *
+ * Return: Something much more useful
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
