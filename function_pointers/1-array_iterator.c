d858404133e59904ddfff003fb930f6cfd14cd50
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-11-24 04:55:03 -0800

    a

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+/**
+ * array_iterators - Something useful
+ * @array: Something more useful
+ * @size: Something more useful
+ * @action: Something more useful
+ *
+ * Return: Something much more useful
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (action != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+	  	action(array[i]);
+		}
+	}
+}
