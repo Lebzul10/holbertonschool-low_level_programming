de099e6900fd349b9b8b774777fd79b4b36bae5f
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-11-24 23:22:24 -0800

    a

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdarg.h>
+/**
+ * print_sign - Something usefull
+ * @n: Something more usefull
+ *
+ * Return: Something much more usefull
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list zor;
+	unsigned int i;
+	int a = 0;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	for (i = 0; i < n; i++)
+	{
+		a = a + va_arg(zor, int);
+	}
+	va_end(zor);
+	return (sum);
+}
