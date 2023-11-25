b891bd6df0d0707f58e22d8d93714dad2b2233db
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-11-24 23:58:14 -0800

    a

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_strings - Something usefull
+ * @n: Something more usefull
+ * @separator: Something more usefull
+ *
+ * Return: Something much more usefull
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list zor;
+	unsigned int i;
+
+	va_start(zor);
+	for (i = 0; i < n; i++)
+	{
+		if (va_arg(zor, char *) ==  NULL)
+		{
+			printf("(nil)");
+		}
+		if (va_arg(zor, char *) != NULL)
+		{
+			printf("%s", va_arg(zor, char*));
+		}
+		if (separator != NULL && i != n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(zor);
+	printf("\n");
+}
