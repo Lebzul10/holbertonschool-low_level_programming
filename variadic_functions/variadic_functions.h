2eed89707721ee6363dd39074020cde1003f3610
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-11-26 10:03:01 -0800

    a

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,9 +1,21 @@
 #ifndef varfun_h
 #define varfun_h
+#include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+/**
+ * struct bomba - Bomba
+ * @f: Bomba
+ * @format: Bomba
+ */
+typedef struct bomba
+{
+	void (*f)(va_list);
+	char format;
+} bomba;
 
 #endif
