7a76b9370f1794ce88aa79ce94e274b96e4724f4
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-12-08 04:17:48 -0800

    a

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * add_node_end - Something useful
+ * @head: Something more useful
+ * @str: Something more useful
+ *
+ * Return: Something much more useful
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *axir, *zor;
+	unsigned int i = 0;
+
+	axir = malloc(sizeof(list_t));
+	if (axir == NULL)
+	{
+		return (NULL);
+	}
+	axir->str = strdup(str);
+	axir->len = strlen(str);
+	zor = *head;
+	while (zor != NULL && zor->next != NULL)
+	{
+		zor = zor->next;
+	}
+	zor->next = axir;
+	axir->next = NULL;
+	return (axir);
+}
