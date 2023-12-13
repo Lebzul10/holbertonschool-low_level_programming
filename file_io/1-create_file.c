f25896406adf01792f29a3c7d9edaddf17fc12ab
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-12-13 06:40:59 -0800

    a

---
+++ file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <string.h>
+#include "main.h"
+/**
+ * create_file - Something useful
+ * @filename: Something more useful
+ * @text_content: Something more useful
+ *
+ * Return: Something much more useful
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, zor, zro;
+
+	if (text_content == NULL)
+	{
+		text_content = "";
+	}
+	fd = open(filename, O_RDONLY | O_WRONLY | O_TRUNC, 0600);
+	zor = strlen(text_content);
+	n = write(fd, text_content, zor);
+	if (n == -1 || filename == NULL)
+	{
+		return (-1);
+	}
+	return (1);
+}
