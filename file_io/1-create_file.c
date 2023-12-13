9c40231f333389e3309f93c9dff010dbd577fee9
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-12-13 06:43:36 -0800

    a

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		text_content = "";
 	}
-	fd = open(filename, O_RDONLY | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	zor = strlen(text_content);
 	zro = write(fd, text_content, zor);
 	if (zro == -1 || filename == NULL)
