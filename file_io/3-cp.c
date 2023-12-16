75723c5bb7b2ca6f0d569f303409d47a1a2ac89c
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-12-16 05:30:50 -0800

    a

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -3,27 +3,48 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <string.h>
+#include <stdio.h>
 /**
- * create_file - Something useful
- * @filename: Something more useful
- * @text_content: Something more useful
+ * main - Something useful
+ * @argc: Something more useful
+ * @argv: Something more useful
  *
  * Return: Something much more useful
  */
-int hesen(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	int fd1, fd2, zor, zro;
-	char *str = malloc(1024);
+	int fd1, fd2, zor, zro, c1, c2;
+	char str[1024];
+
 	if (argc != 3)
 	{
 		exit(97);
 	}
-	fd1 = open(argv[1], O_READONLY | O_CREAT);
+	fd1 = open(argv[1], O_RDONLY);
 	zor = read(fd1, str, 1024);
-	close(fd);
-	fd2 = open(argv[2], O_WRONLY | O_CREAT);
-	zro = write(fd2, str, 1024);
-	close(fd2);
+	c1 = close(fd1);
+	if (zor == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	fd2 = open(argv[2], O_RDWR | O_TRUNC | O_CREAT, 0664);
+	zro = write(fd2, str, zor);
+	c2 = close(fd2);
+	if (zro == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	if (c1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
+		exit(100);
+	}
+	if (c2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
+		exit(100);
+	}
 	return (0);
-	
 }
