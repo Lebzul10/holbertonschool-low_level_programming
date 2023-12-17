90a520d8a88e06f04e8c75516997b4184d6bb428
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-12-17 03:35:16 -0800

    a

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "hash_tables.h"
+/**
+ * key_index - Something usefull
+ * @key: Something more usefull
+ * @size: Something more usefull
+ *
+ * Return: Something much more usefull
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int zor, index;
+
+	zor = hash_djb2((unisgned char *)key);
+	index = zor % size;
+	return (index);
+}
