f95574d60c335fed8d59cb472260afecc7bbf461
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-12-17 04:24:18 -0800

    a

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "hash_tables.h"
+/**
+ * hash_table_set - Something usefull
+ * @ht: Something more usefull
+ * @key: Something more usefull
+ * @value: Something more usefull
+ *
+ * Return: Something much more usefull
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *zor;
+
+	if (ht == NULL)
+	  {
+		return (0);
+	  }
+	index = key_index((unsigned char *)key, ht->size);
+	zor = malloc(sizeof(hash_node_t));
+	if (zor == NULL)
+	{
+		return (0);
+	}
+	zor->key = strdup(key);
+	zor->value = strdup(value);
+	if (ht->array[index] == NULL)
+	  {
+		ht->array[index] = zor;
+		zor->next = NULL;
+	  }
+	else
+	  {
+		while (ht->array[index] != NULL)
+		  {
+			ht->array[index] = ht->array[index]->next;
+		  }
+		zor->next = NULL;
+		ht->array[index] = zor;
+	  }
+	return (1);
+}
