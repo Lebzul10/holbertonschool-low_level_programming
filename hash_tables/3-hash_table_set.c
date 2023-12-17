91d4e6335a3e12b9bea46462f9e701e886d9bd44
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-12-17 04:34:51 -0800

    a

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -36,6 +36,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	  {
 		while (ht->array[index] != NULL)
 		  {
+			if (strcmp(ht->array[index]->key, key) == 0)
+			  {
+				ht->array[index]->value = strdup(value);
+				return (1);
+			  }
 			ht->array[index] = ht->array[index]->next;
 		  }
 		zor->next = NULL;
