3277e41290b50a0d3c2f354bf94a99454efafe62
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-12-17 05:10:28 -0800

    a

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -16,9 +16,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *zor, *zro;
 
 	if (ht == NULL)
-	  {
+	{
 		return (0);
-	  }
+	}
 	index = key_index((unsigned char *)key, ht->size);
 	zor = malloc(sizeof(hash_node_t));
 	if (zor == NULL)
@@ -28,24 +28,24 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	zor->key = strdup(key);
 	zor->value = strdup(value);
 	if (ht->array[index] == NULL)
-	  {
+	{
 		ht->array[index] = zor;
 		zor->next = NULL;
-	  }
+	}
 	else
-	  {
+	{
 		zro = ht->array[index];
 		while (ht->array[index] != NULL)
-		  {
+		{
 			if (strcmp(ht->array[index]->key, key) == 0)
-			  {
+			{
 				ht->array[index]->value = strdup(value);
 				return (1);
-			  }
+			}
 			ht->array[index] = ht->array[index]->next;
-		  }
+		}
 		zor->next = zro;
 		ht->array[index] = zor;
-	  }
+	}
 	return (1);
 }
