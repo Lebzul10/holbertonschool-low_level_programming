20321ca3638c0b12b4c05b9224e6906a1fd15334
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-12-17 05:32:21 -0800

    a

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -14,9 +14,17 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int i;
 
 	i = key_index((unsigned char *)key, ht->size);
-	if (ht->array[i]->key == NULL)
+	if (ht->array[i]->key == NULL || ht == NULL)
 	{
 		return (NULL);
+	}
+	while (ht->array[i] != NULL)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			return (ht->array[i]->value);
+		}
+		ht->array[i] = ht->array[i]->next;
 	}
 	return (ht->array[i]->value);
 }
