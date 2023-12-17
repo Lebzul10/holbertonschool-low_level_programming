122f7210cba1bc96ec97cdd4b6e256039520ac4b
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-12-17 05:56:51 -0800

    a

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+/**
+ * print_sign - Something usefull
+ * @n: Something more usefull
+ *
+ * Return: Something much more usefull
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i = 0, size = ht->size;
+
+	if (ht == NULL)
+	  {
+		printf("{}\n");
+		return;
+	  }
+	printf("{");
+	while (size != 0)
+	{
+		if (ht->array[i] != NULL)
+		{
+			printf("'%s': '%s', ", ht->array[i]->key, ht->array[i]->value);
+			size--;
+		}
+		i++;
+	}
+	printf("}");
+}
