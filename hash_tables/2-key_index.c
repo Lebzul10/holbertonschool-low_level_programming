7c5dee3dc42edf7919423529e7d918bea03b8435
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-12-17 03:36:46 -0800

    a

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -12,7 +12,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int zor, index;
 
-	zor = hash_djb2((unisgned char *)key);
+	zor = hash_djb2((unsigned char *)key);
 	index = zor % size;
 	return (index);
 }
